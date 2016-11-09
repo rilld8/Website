5c02fa4263e0c5ed8ca748df41ed1f40548dcb1b
rilld8 <rilld8@gmail.com>
2016-11-09 08:25:53 +0100

    adding website components

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'nickname': 'Karol'}
     return render_template('Index.html', title='Karol Sz.', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                           title='Sign In!', form=form)
---
+++ config.py
@@ -0,0 +1,2 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'Hornet1303Aa'
